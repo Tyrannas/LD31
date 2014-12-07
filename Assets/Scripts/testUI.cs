dc1bdf99d5dd6c0027a94973559d287fb76d5f18
KerDelos <dfdelmas@gmail.com>
2014-12-07 13:16:15 +0100

    health bar

--- Assets/Scripts/testUI.cs
+++ Assets/Scripts/testUI.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 
 public class testUI : MonoBehaviour {
-
+	
+	//public Transform scrollr;
 	// Use this for initialization
+	public int test;
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKeyDown(KeyCode.L))
+		{
+			Debug.Log ("L pushed");
+			GetComponent<RectTransform>().sizeDelta = new Vector2(test,GetComponent<RectTransform>().sizeDelta.y);
+		}
 	}
 }
