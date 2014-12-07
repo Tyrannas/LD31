d012e7003a13cc32a60904c521a27437bc98ccf4
KerDelos <dfdelmas@gmail.com>
2014-12-07 19:21:21 +0100

    sounds script works mothafocka

---
+++ Assets/Scripts/soundsScriptHero.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class soundsScriptHero : MonoBehaviour {
+
+	public AudioClip attackSound;
+	public AudioSource audiosc;
+	// Use this for initialization
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.E))
+			playAttack();
+	}
+	
+	public void playAttack()
+	{
+		audio.PlayOneShot(attackSound);
+	}
+}
