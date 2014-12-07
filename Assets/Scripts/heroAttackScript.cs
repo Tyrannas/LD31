9974da395bc51d6d519b7a786788a74e84cdf3d0
KerDelos <dfdelmas@gmail.com>
2014-12-07 01:14:37 +0100

    attack working !

--- Assets/Scripts/heroAttackScript.cs
+++ Assets/Scripts/heroAttackScript.cs
@@ -5,6 +5,13 @@ public class heroAttackScript : MonoBehaviour {
 	
 	int pv;
 	int puissance;
+	public float attackLength;
+	float timeLeftAttacking;
+	
+	public Transform weaponHero;
+	public Animator animhero;
+	
+	bool attacking = false;
 	
 	public void setPv(int nb)
 	{
@@ -14,6 +21,44 @@ public class heroAttackScript : MonoBehaviour {
 	{
 		puissance=nb;
 	}
+	
+	void Update () {
+		//attaque du personnage
+		if(Input.GetKeyDown(KeyCode.Z) && !attacking)
+		{
+			attack();
+		}
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			weaponHero.collider2D.enabled=false;
+		}	
+	}
+	void FixedUpdate()
+	{
+		proceedAttack();
+	}
+	
+	void attack()
+	{
+		attacking = true;
+		weaponHero.collider2D.enabled=true;
+		timeLeftAttacking = attackLength;
+		animhero.Play ("attackHero");
+		
+		
+	}
+	
+	void proceedAttack()
+	{
+		timeLeftAttacking -= Time.deltaTime;
+		if(timeLeftAttacking <= 0)
+		{
+			animhero.Play ("idleHero");
+			attacking=false;
+			weaponHero.collider2D.enabled=false;
+		}
+	}
+
 	
 	
 }
