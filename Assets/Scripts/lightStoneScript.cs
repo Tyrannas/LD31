7b2ae104eac221fafd2195bc65891239ed474651
KerDelos <dfdelmas@gmail.com>
2014-12-08 02:33:01 +0100

    NEW AWESOME GAMEPLAY MECHANIC !!

---
+++ Assets/Scripts/lightStoneScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightStoneScript : MonoBehaviour {
+
+	bool isActive=false;
+
+	void Update () {
+	
+	}
+	
+	
+	void activate()
+	{
+		GameObject cam = GameObject.Find("Main Camera");
+		cam.transform.parent.GetComponent<lightStoneScript>().Deactivate();
+		isActive=true;
+		cam.transform.parent = this.transform;
+		cam.transform.localPosition = new Vector3(0,0,cam.transform.position.z);
+	}
+	public void Deactivate()
+	{
+		isActive=false;	
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.collider.name=="Hero" && !isActive)
+		{
+			activate();	
+		}
+		
+	}
+	
+	
+}
